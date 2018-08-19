5d8f9fe4a806217833f85942998d35177dbd6951
yalinisen <5222149+yalinisen@users.noreply.github.com>
2018-08-19 07:18:20 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -132,35 +132,5 @@
 
   //addn by yal
 
-/*
-var demo = {
-    serviceUrl: "https://api.hspconsortium.org/hspcdemo/open",
-    patientId: "BILIBABY"
-};
 
-// Create a FHIR client (server URL, patient id in `demo`)
-var smart = FHIR.client(demo),
-    pt = smart.patient;
-
-// Create a patient banner by fetching + rendering demographics
-smart.patient.read().then(function(pt) {
-  displayPatient (pt);
-});
-
-// A more advanced query: search for active Prescriptions, including med details
-smart.patient.api.fetchAll({type: "Observation"})
-.then(function(results, refs) {
-  results.forEach(function(observation){
-    displayObservation(observation);
-  });
-});
-function displayObservation (observation) {
-  var table = document.getElementById("obs_table");
-  var row = table.insertRow(1);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  cell1.innerHTML = observation.code.coding[0].code;
-  cell2.innerHTML = observation.valueQuantity.value;
-}
-*/
 })(window);
