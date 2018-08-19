eff2c4ebbab77cc9789140ef864979e36d79b698
yalinisen <5222149+yalinisen@users.noreply.github.com>
2018-08-19 09:57:52 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -20,14 +20,7 @@
                               'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
                       }
                     }
-      //    type: 'DiagnosticReport',
-      //              query: {}
-       //           });
-
-             
-  type: 'Condition',
-  category: 'problem',
- // clinicalstatus: 'active'
+    
 });
         $.when(pt, obv).fail(onError);
 
