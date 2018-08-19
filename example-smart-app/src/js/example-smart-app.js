07c2f1bd42a83a2cfaacdf477288769ea961aad5
yalinisen <5222149+yalinisen@users.noreply.github.com>
2018-08-19 12:26:04 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -156,7 +156,21 @@ function displayObservation (observation) {
               $("#med_list").append(row);
             })
   //end of addn
+        
 /*
+
+ var obvmed = smart.patient.api.fetchAll({
+                    type: 'Medication',
+                    query: {
+                    //  code: {
+                   //     $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+                  //            'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
+                   //           'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                      }
+                    }
+
+
+
 var demo = {
     serviceUrl: "https://api.hspconsortium.org/hspcdemo/open",
     patientId: "BILIBABY"
