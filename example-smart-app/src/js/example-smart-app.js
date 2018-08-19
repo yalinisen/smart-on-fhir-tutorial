9a97efa052da8ec88008287219240daf75f049c0
yalinisen <5222149+yalinisen@users.noreply.github.com>
2018-08-19 09:27:51 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -131,6 +131,40 @@
   };
 
   //addn by yal
+/*
+function displayPatient (pt) {
+  document.getElementById('patient_name').innerHTML = getPatientName(pt);
+}
 
+function displayObservation (observation) {
+  var table = document.getElementById("obs_table");
+  var row = table.insertRow(1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell1.innerHTML = observation.code.coding[0].code;
+  cell2.innerHTML = observation.valueQuantity.value;
+}
+
+var demo = {
+    serviceUrl: "https://api.hspconsortium.org/hspcdemo/open",
+    patientId: "BILIBABY"
+};
+
+// Create a FHIR client (server URL, patient id in `demo`)
+var smart = FHIR.client(demo),
+    pt = smart.patient;
+
+// Create a patient banner by fetching + rendering demographics
+smart.patient.read().then(function(pt) {
+  displayPatient (pt);
+});
+*/
+// A more advanced query: search for active Prescriptions, including med details
+smart.patient.api.fetchAll({type: "Observation"})
+.then(function(results, refs) {
+  results.forEach(function(observation){
+    displayObservation(observation);
+  });
+});
 
 })(window);
